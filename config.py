d054daace123663ec6997587071d6247c4bfa6b0
Atika <143552531+atikanusrat@users.noreply.github.com>
2025-07-23 15:51:32 -0400

    Update config.py to show config template

--- config.py
+++ config.py
@@ -1,4 +1,4 @@
 # config.py - Configuration file for Commodity Network Analysis
 
-# Census Bureau API Key 
-CENSUS_API_KEY = "053d5b12246c70a40f3a420b49176bef59b4eeea"
+# config_template.py
+CENSUS_API_KEY = "YOUR_API_KEY_HERE"
